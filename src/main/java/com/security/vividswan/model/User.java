1dd471a7d7a4e1def916284907e3b7991b26aa0f
vividswan <vividswan@naver.com>
2020-09-10 00:07:20 +0900

    Create User.java

---
+++ src/main/java/com/security/vividswan/model/User.java
@@ -0,0 +1,30 @@
+package com.security.vividswan.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
