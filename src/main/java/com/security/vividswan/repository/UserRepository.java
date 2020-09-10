ccb53e7b11f807c79395dd9e33b374d44130e18f
vividswan <vividswan@naver.com>
2020-09-11 00:15:05 +0900

    Create UserRepository.java

---
+++ src/main/java/com/security/vividswan/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.security.vividswan.repository;
+
+import com.security.vividswan.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
