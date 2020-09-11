a1d32df9c2d37754a6b4305b85955f6197693c6b
vividswan <vividswan@naver.com>
2020-09-11 21:31:41 +0900

    Update SecurityConfig.java

--- src/main/java/com/security/vividswan/config/SecurityConfig.java
+++ src/main/java/com/security/vividswan/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.security.vividswan.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
