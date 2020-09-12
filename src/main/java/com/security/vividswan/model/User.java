20c5e698eeff9df762f28c315602332e75e5a2a7
vividswan <vividswan@naver.com>
2020-09-12 21:47:11 +0900

    Update User.java

--- src/main/java/com/security/vividswan/model/User.java
+++ src/main/java/com/security/vividswan/model/User.java
@@ -24,6 +24,10 @@ public class User {
     private String password;
     private String email;
     private String role;
+
+    private String provider;
+    private String providerId;
+
     @CreationTimestamp
     private Timestamp createDate;
 
